c40f7dad45118bb8fd1323c49e66f08b3a9ca01b
ding <dstonecool@163.com>
2016-01-12 23:53:01 +0800

    Add clas List which extends python list

---
+++ List.py
@@ -0,0 +1,60 @@
+class List(list):
+	def __init__(self,ls = []):
+		list.__init__([])
+		self.extend(ls)
+
+	def index(self,method = None):
+		for i in range(len(self)):
+			if method(self[i]):
+				return i
+		return -1
+
+	def split(self,Separator = None,method = None,included = None):
+
+		compare = lambda x:x==Separator
+		if method:
+			compare = method
+
+		def push(group,ls):
+			if len(ls):group.append(ls)
+			return group
+
+		next = lambda x:x+1 
+		if included:
+			next = lambda x:x
+
+		group = []
+		pos =  0
+		for i in range(len(self)):
+			if compare(self[i]):
+				group = push(group,self[pos:i])
+				pos = next(i)
+
+		return push(group,self[pos:])
+
+	def group(self,method = None):
+		legal = []
+		ilegal = []
+		judge = lambda x:True
+		if method:
+			judge = method
+		for i in self:
+			if judge(i):
+				legal.append(i)
+			else:
+				ilegal.append(i)
+		return legal,ilegal
+
+
+if __name__ == '__main__':
+
+	ls = List()
+	ls = List([1,2,6,3,1,4,1,5,1,6,7,1])
+
+	print ls.split(1)
+	print ls.split(1,included = True)
+	print ls.split()
+	print ls.split(method = lambda x: x%2)
+	print ls.split(method = lambda x:x==6)	
+	print ls.group(lambda x:x>=4)
+
